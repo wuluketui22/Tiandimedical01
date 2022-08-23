afd97595a9b226b823ea86dabb23b516e722ad31
wuluketui22 <xt635238638@163.com>
2022-08-23 17:44:29 +0800

    添加注解启动类

---
+++ system-consumer/src/main/java/com/whackon/witmed/SystemConsumerTarter.java
@@ -0,0 +1,15 @@
+package com.whackon.witmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+
+public class SystemConsumerTarter {
+	public static void main(String[] args) {
+		SpringApplication.run(SystemConsumerTarter.class, args);
+	}
+}
