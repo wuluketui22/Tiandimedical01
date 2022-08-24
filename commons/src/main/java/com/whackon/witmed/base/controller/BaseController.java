c290b16f2a21bf6e0e84906ef453d9bdacbd464d
Yan <yzw648784087@163.com>
2022-08-24 21:10:12 +0800

    修改req和resp所引包

--- commons/src/main/java/com/whackon/witmed/base/controller/BaseController.java
+++ commons/src/main/java/com/whackon/witmed/base/controller/BaseController.java
@@ -1,10 +1,9 @@
 package com.whackon.witmed.base.controller;
 
-import cn.hutool.http.server.HttpServerRequest;
-import cn.hutool.http.server.HttpServerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -23,9 +22,9 @@ import javax.servlet.http.HttpSession;
 @Controller("baseController")
 public class BaseController {
 	@Autowired
-	protected HttpServerRequest request;
+	protected HttpServletRequest request;
 	@Autowired
-	protected HttpServerResponse response;
+	protected HttpServletResponse response;
 	@Autowired
 	protected HttpSession session;
 }
