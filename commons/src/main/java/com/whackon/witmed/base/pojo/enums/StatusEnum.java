d5aa40d50bf92853c7c954a508470abe5fed5723
Yan <yzw648784087@163.com>
2022-08-24 19:21:16 +0800

    创建系统状态枚举类

--- commons/src/main/java/com/whackon/witmed/base/pojo/enums/StatusEnum.java
+++ commons/src/main/java/com/whackon/witmed/base/pojo/enums/StatusEnum.java
@@ -1,4 +1,5 @@
 package com.whackon.witmed.base.pojo.enums;
+
 /**
  * <b>系统状态枚举信息</b>
  *
@@ -6,6 +7,8 @@ package com.whackon.witmed.base.pojo.enums;
  * @date 2022/8/24
  */
 public enum StatusEnum {
+	STATUS_ENABLE(BaseEnumConstants.STATUS_ENABLE,"启用"),
+	STATUS_DISABLE(BaseEnumConstants.STATUS_DISABLE,"禁用");
 	;
 	private String code;            // 状态编码
 	private String remark;          // 状态说明
