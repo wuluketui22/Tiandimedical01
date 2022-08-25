42bb94a3cae34e3016ef1447cfd9e0ac8736b10e
wuluketui22 <xt635238638@163.com>
2022-08-25 11:19:41 +0800

    改模块名

--- commons/src/main/java/com/whackon/witmed/base/controller/BaseController.java
+++ commons/src/main/java/com/whackon/witmed/base/controller/BaseController.java
@@ -2,29 +2,23 @@ package com.whackon.witmed.base.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
  * <b>基础控制层类</b>
- * <p>
- *     基础控制层类依赖注入了如下属性：<br/>
- *     HttpServerRequest
- *     HttpServerResponse
- *     HttpSession
- *     本项目所有功能控制层类必须继承于本基础控制层类
  *
- * </p>
- * @author ASUS
- * @data 2022-08-24
+ * @author DELL
+ * @date 2022/8/25
  */
 @Controller("baseController")
 public class BaseController {
-	@Autowired
-	protected HttpServletRequest request;
-	@Autowired
-	protected HttpServletResponse response;
-	@Autowired
-	protected HttpSession session;
+		@Autowired
+		protected HttpServletRequest request;
+		@Autowired
+		protected HttpServletResponse response;
+		@Autowired
+		protected HttpSession session;
 }
