ce1eaaa8005ca98972365b0e2617f24762c5afa4
Yan <yzw648784087@163.com>
2022-09-12 22:58:02 +0800

    创建实体信息和Dao层

---
+++ tiandimedical-data-commons/src/main/java/com/whackon/witmed/patient/patient/dao/PatAllHInfoDao.java
@@ -0,0 +1,14 @@
+package com.whackon.witmed.patient.patient.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.whackon.witmed.patient.patient.pojo.entity.PatAllHInfo;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <b>天地医疗信息平台 - 患者功能 - 患者过敏史详情 Dao层<b/>
+ * @author yan
+ * @date 2022/9/12
+ */
+@Repository
+public interface PatAllHInfoDao extends BaseMapper<PatAllHInfo> {
+}
---
+++ tiandimedical-data-commons/src/main/java/com/whackon/witmed/system/dictionary/dao/DataDictionaryDao.java
@@ -0,0 +1,14 @@
+package com.whackon.witmed.system.dictionary.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.whackon.witmed.system.dictionary.pojo.entity.DataDictionary;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <b>天地医疗信息平台 - 系统功能 - 数据字典 Dao层<b/>
+ * @author yan
+ * @date 2022/9/12
+ */
+@Repository
+public interface DataDictionaryDao extends BaseMapper<DataDictionary> {
+}
